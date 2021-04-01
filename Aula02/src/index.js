c88e0efb5ab7875759174623ead71b99d3eafa9c
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2021-04-01 09:11:29 -0300

    aula 2

---
+++ Aula02/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/css/style.css';
+import './assets/css/reset.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
